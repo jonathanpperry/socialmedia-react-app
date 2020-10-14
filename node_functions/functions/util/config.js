1ef6851130d079212390141c9c3fc73b92d26820
Jonathan Perry <jpper@umich.edu>
2020-10-14 10:14:39 +0900

    Combined client and back-end functions into new private repository

---
+++ node_functions/functions/util/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apiKey: "AIzaSyDB-LHvYwEEgN9vC49_xP5DMtG_yg2iGAA",
+  authDomain: "socialape-710af.firebaseapp.com",
+  databaseURL: "https://socialape-710af.firebaseio.com",
+  projectId: "socialape-710af",
+  storageBucket: "socialape-710af.appspot.com",
+  messagingSenderId: "215130014199",
+};
